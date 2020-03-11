df3faa25af5a785a602189e8da2f9aefbecc3c45
Ryanlys <ryanlam321@hotmail.com>
2020-03-10 20:07:00 -0700

    added canned responses

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import General,PersonOrLocation,Chat
 
-words = input('Hi. I am bot. What can I help you with today? \n')
+words = input('Hey, what did you want to talk about? \n')
 branch = PersonOrLocation.determineBranch(words.split())
 Chat.setBranch(branch)
 Chat.Chat(words)
