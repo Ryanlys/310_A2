12d154aa9614e70230aee0e7b51bfd6100547970
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2020-03-27 20:09:12 -0700

    Add files via upload
    findPersonKeywords now returns a Person object.
    Person object has attributes: name, isIntroduced, relationshipStatus, and lengthOfRelationship.
    Printing a Person will just print the person's name.

--- PersonOrLocation.py
+++ PersonOrLocation.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python
 
+# Person is an object that stores information of a person 
+class Person:
+    def __init__(self, personName):
+        self.personName = personName
+        self.isIntroduced = False   # This variable tells the program if the user has introduced the person to the the software
+        self.relationshipStatus = None # This variable tells the program the user's relationship status with the person
+        self.lengthOfRelationship = None # This variable tells the program the length of relationship between the user and the
+        
+    def __repr__(self):
+        return self.personName
+
 # findPersonKeywords is a function that takes a list of words (or user's "chat" in this context)
 # returns the list of person names in the given list
 def findPersonKeywords(words):
     persons = []
+    personsObject = []
 
     if 'my' in words:
         myIndex = words.index('my')
         persons.append(words[myIndex + 1])
+        personsObject.append(new Person(words[myIndex + 1]))
 
     if 'met' in words:
         myIndex = words.index('met')
         persons.append(words[myIndex + 1])
+        personsObject.append(new Person(words[myIndex + 1]))
         
     if 'with' in words:
         withIndex = words.index('with') # returns the index of the word 'with' in the list of words
 
         if words[withIndex + 1].lower() == 'my' and words[withIndex + 2] not in persons:
             persons.append(words[withIndex + 2])
+            personsObject.append(new Person(words[withIndex + 2]))
         else:
             personName = words[withIndex + 1]
             personName = personName.capitalize()
             if personName not in persons:
                 persons.append(personName)
+                personsObject.append(new Person(personName))
 
     if 'to' in words:
         toIndex = words.index('to')
@@ -30,11 +46,13 @@ def findPersonKeywords(words):
         if (words[toIndex - 1].lower() != 'went') and (words[toIndex - 1].lower() != 'how') and (words[toIndex - 1].lower() != 'go') and (words[toIndex - 1].lower() != 'visit'):
             if words[toIndex + 1].lower() == 'my' and words[toIndex + 2] not in persons:
                 persons.append(words[toIndex + 2])
+                personsObject.append(new Person(words[toIndex + 2]))
             else:
                 personName = words[toIndex + 1]
                 personName = personName.capitalize()
                 if personName not in persons:
                     persons.append(personName)
+                    personsObject.append(new Person(personName))
             
     return persons
 
@@ -54,7 +72,7 @@ def findLocationKeywords(words):
     return locations
 
 # PersonOrLocation is a function that takes a string (or user's "chat" in this context)
-# Returns a dictionary data type with the list of people's names and locations.
+# Returns a dictionary data type with the list of Person objects and locations.
 def PersonOrLocation(string):
     words = string.lstrip().rstrip().split() # removes unnecessary left/right spaces and returns a list of words in the string
     
