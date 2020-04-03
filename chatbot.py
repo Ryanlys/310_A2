fdc7d8a59af09cacbdc859989afcc70bb4d14b71
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2020-04-02 20:40:53 -0700

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
 
+'''
+    PLEASE NOTE:
+        ONLY RUNS ON PYTHON 3+.
+        Python 2's GUI library (tkinter) uses a slightly-different library name and hence will crash upon loading 'tkinter' library, if run on Py2.
+'''
+
 import PersonOrLocation
 import questionBuilder
-import pythonptk
+import tkinter # GUI library
+import tkinter.font
+from tkinter import ttk
 
+# Person is an object that stores information of a person 
+class Person:
+    def __init__(self, personName):
+        self.personName = personName
+        self.isIntroduced = False   # This variable tells the program if the user has introduced the person to the the software
+        self.relationshipStatus = None # This variable tells the program the user's relationship status with the person
+        self.lengthOfRelationship = None # This variable tells the program the length of relationship between the user and the
+        
+    def __repr__(self):
+        return self.personName
+    
 class Category:
     def __init__(self, categoryName):
         self.categoryName = categoryName
@@ -30,6 +49,35 @@ class Relationship:
     def __repr__(self):
         return self.relationshipName
 
+# --- GUI stuffs
+def GUI_btnReply_onClick():
+    pass
+
+mainWindow = tkinter.Tk()
+
+font_verdana12 = tkinter.font.Font(family = "Verdana", size = 12)
+
+scrollbar = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL)
+listbox = tkinter.Listbox(mainWindow, yscrollcommand = scrollbar.set)
+textInput = tkinter.Entry(mainWindow, font = font_verdana12)
+btnReply = tkinter.Button(mainWindow, font = font_verdana12, text = "Reply >", command = GUI_btnReply_onClick)
+
+for i in range(100):
+    listbox.insert(tkinter.END, str(i))
+
+listbox.grid(row = 0, columnspan = 1, rowspan = 2, sticky = "nw")
+scrollbar.grid(row = 0, column = 1, sticky = "nse")
+textInput.grid(row = 1, column = 0, sticky = "sw")
+btnReply.grid(row = 1, column = 1, sticky = "se")
+
+scrollbar.config(command = listbox.yview)
+
+mainWindow.columnconfigure(1, weight = 1)
+mainWindow.rowconfigure(1, weight = 1)
+
+mainWindow.mainloop()
+# ---
+
 # Add the Categories into the "database"
 happyCategory = Category("Happy")
 neutralCategory = Category("Neutral")
@@ -48,17 +96,21 @@ matchingCategories = []
 
 name = input("\n\nHey there, it's bot. Mind identifying yourself? What should I call you?\n\t> ")
 name = name.lstrip().rstrip().split(" ")[-1] # Extracts the last word in the sentence
+name = name[0].upper() + name[1:]
 sentence = input("Hello " + name + ", how are ya feeling today?\n\t> ")
                       
 ### Testing
 while True:
     words = sentence.split(" ")
-    matchingCategories = []
+    locations = []
     
     for word in words:
         for category in categories:
             if word in category.keywords:
                 matchingCategories.append(category.categoryName)
+
+    if len(matchingCategories) > 1:
+        matchingCategories = matchingCategories[1:]
 
     for personName in PersonOrLocation.PersonOrLocation(sentence)['Persons']:
         if personName not in persons:
--- questionBuilder.py
+++ questionBuilder.py
@@ -2,6 +2,11 @@
 
 import random
 
+def removePersonFromList(person, persons):
+    index = persons.index(person)
+    persons.pop(index)
+    return persons
+
 def buildQuestion(matchingCategories, location=None, persons=None):
     reply = ""
 
@@ -10,60 +15,125 @@ def buildQuestion(matchingCategories, location=None, persons=None):
     sadUnderstandingWords = ["Oh no!", "Aiyoo...", "I feel terrible for you.", "That's not a good thing to hear."]
     happyUnderstandingWords = ["That's great to hear!", "I'm glad to hear that!", "Ohh that's nice to know.", "That's good."]
 
-    askFeelingQuestions = ["How did you feel about", "How was", "Did you enjoy"]
-    askLocationQuestions = ["Where did you go to", "Did you go anywhere interesting"]
-    askPersonQuestions = ["Could you tell me more about", "Who's", "Ya mind introducing me to", "How would you describe"]
-    askMoreFeelingQuestions = ["Tell me more about why you're feeling", "Could you be more specific by feeling"]
+    askLocationQuestions = ["Where did you go to today?", "Did you do anywhere interesting?"]
+    askMoreLocationQuestions = ["What made you you go to %s in the first place?", "Why did you go to the %s?", "How was %s?", "Did you enjoy going to %s?"]
+    askPersonQuestions = ["Could you tell me more about %s?", "Who's %s?", "Would you mind telling me a bit about %s?", "Who is %s to you?"]
+    askMoreFeelingQuestions = ["Tell me more why you're feeling %s right now.", "Could you be more specific by feeling %s?", "Since you're feeling %s, have you been doing anything else lately?"]
     askGeneralQuestions = ["Is there anything else you would want to talk about?"]
+    askKeepGoing = ["Tell me more", "What makes you feel this way?", "What else do you think would help you at this point?", "Go on, I'm listening.", "I'm here. Tell me more.", "Has there been anything else bothering you lately?"]
                                
     if len(matchingCategories) == 0:
-        if len(location) == 0:
-            if len(persons) != 0:
-                reply += " with "
-                for person in persons:
-                    reply += person.personName
-            else:
-                reply = random.choice(askLocationQuestions)
-                           
-            reply += "?"
-            return reply
-                           
-        else:
-            if len(persons) == 0:
-                reply += random.choice(neutralUnderstandingWords) + " Who did you go to %s with?" % location[0]
-            else:
+        if location is None or len(location) == 0:
+            if persons is None or len(persons) != 0:
                 for person in persons:
                     if not person.isIntroduced:
-                        reply = random.choice(askPersonQuestions)
-                        reply += " %s?" % person.personName
+                        reply = random.choice(askPersonQuestions) % person.personName
                         relationshipStatus = input("\n" + reply + "\n\t> ").lstrip().rstrip().split()
 
                         while 'my' not in relationshipStatus:
                             reply = random.choice(askPersonQuestions) + (" %s again?" % person.personName)
                             relationshipStatus = input("\nUmm sorry, I don't understand your reply just now. " + reply + "\n\t> ").lstrip().rstrip().split()
+                            return reply
                             
                         if 'my' in relationshipStatus:
                             relationshipStatus = " ".join(relationshipStatus[(relationshipStatus.index('my') + 1):]) # Joins all words after 'my'
                             person.isIntroduced = True
                             person.relationshipStatus = relationshipStatus
-                            reply = random.choice(neutralUnderstandingWords) + ".. Your " + relationshipStatus + ". "
+                            persons = removePersonFromList(person, persons)
+                            reply = "Your " + relationshipStatus + ". " + random.choice(neutralUnderstandingWords)
      
                     if person.lengthOfRelationship is None:
                         lengthOfRelationship = input(reply + "How long have you known %s for?\n\t> " % person.personName)
                         person.lengthOfRelationship = lengthOfRelationship
                         reply = random.choice(neutralUnderstandingWords) + random.choice(askMoreFeelingQuestions)
+                        return reply
                         
                     else:
                         reply += " " + random.choice(askGeneralQuestions)
-                               
-            return reply
-    else:
-        if matchingCategories[0] == "Depressed":
-            reply = random.choice(sadUnderstandingWords)
-        elif matchingCategories[0] == "Neutral":
-            reply = random.choice(neutralUnderstandingWords)
-        elif matchingCategories[0] == "Happy":
-            reply = random.choice(happyUnderstandingWords)
+                        return reply
 
-        reply += " " + random.choice(askMoreFeelingQuestions) + " " + matchingCategories[0].lower() + "."
-        return reply
+                reply = random.choice(neutralUnderstandingWords) + " " + random.choice(askKeepGoing)
+                return reply
+            else:
+                reply = random.choice(askLocationQuestions)           
+            return reply
+                           
+        else:
+            if len(persons) == 0:
+                input(("\n" + random.choice(askMoreLocationQuestions) + "\n\t>") % location[0])
+                reply += random.choice(neutralUnderstandingWords) + " Who did you go to %s with?" % location[0]
+                return reply
+            else:
+                input(("\n" + random.choice(askMoreLocationQuestions) + "\n\t> ") % location[0])
+                for person in persons:
+                    if not person.isIntroduced:
+                        reply = random.choice(askPersonQuestions) % person.personName
+                        relationshipStatus = input("\n" + reply + "\n\t> ").lstrip().rstrip().split()
+
+                        while 'my' not in relationshipStatus:
+                            reply = random.choice(askPersonQuestions) + (" %s again?" % person.personName)
+                            relationshipStatus = input("\nUmm sorry, I don't understand your reply just now. " + reply + "\n\t> ").lstrip().rstrip().split()
+                            return reply
+                            
+                        if 'my' in relationshipStatus:
+                            relationshipStatus = " ".join(relationshipStatus[(relationshipStatus.index('my') + 1):]) # Joins all words after 'my'
+                            person.isIntroduced = True
+                            persons = removePersonFromList(person, persons)
+                            person.relationshipStatus = relationshipStatus
+                            reply = "Your " + relationshipStatus + ". " + random.choice(neutralUnderstandingWords)
+     
+                    if person.lengthOfRelationship is None:
+                        lengthOfRelationship = input(reply + "How long have you known %s for?\n\t> " % person.personName)
+                        person.lengthOfRelationship = lengthOfRelationship
+                        reply = random.choice(neutralUnderstandingWords) + random.choice(askMoreFeelingQuestions)
+                        return reply
+                        
+                    else:
+                        reply += " " + random.choice(askGeneralQuestions)
+                        return reply
+
+                reply = random.choice(neutralUnderstandingWords) + " " + random.choice(askKeepGoing)
+                return reply
+    else:
+        if len(location) != 0:
+            return (("\n" + random.choice(askMoreLocationQuestions)) % location[0])
+        else:
+            if len(persons) != 0:
+                for person in persons:
+                    if not person.isIntroduced:
+                        reply = ("By the way, " + random.choice(askPersonQuestions)) % person.personName
+                        relationshipStatus = input("\n" + reply + "\n\t> ").lstrip().rstrip().split()
+
+                        while 'my' not in relationshipStatus:
+                            reply = random.choice(askPersonQuestions) + (" %s again?" % person.personName)
+                            relationshipStatus = input("\nUmm sorry, I don't understand your reply just now. " + reply + "\n\t> ").lstrip().rstrip().split()
+                            return reply
+                            
+                        if 'my' in relationshipStatus:
+                            relationshipStatus = " ".join(relationshipStatus[(relationshipStatus.index('my') + 1):]) # Joins all words after 'my'
+                            person.isIntroduced = True
+                            persons = removePersonFromList(person, persons)
+                            person.relationshipStatus = relationshipStatus
+                            reply = "Your " + relationshipStatus + ". " + random.choice(neutralUnderstandingWords)
+     
+                    if person.lengthOfRelationship is None:
+                        lengthOfRelationship = input(reply + "How long have you known %s for?\n\t> " % person.personName)
+                        person.lengthOfRelationship = lengthOfRelationship
+                        reply = (random.choice(neutralUnderstandingWords) + random.choice(askMoreFeelingQuestions)) % matchingCategories[0].lower()
+                        return reply
+                        
+                    else:
+                        reply = random.choice(neutralUnderstandingWords) + " " + random.choice(askKeepGoing)
+                        return reply
+
+            else:   
+                if matchingCategories[0] == "Depressed":
+                    reply = random.choice(sadUnderstandingWords)
+                elif matchingCategories[0] == "Neutral":
+                    reply = random.choice(neutralUnderstandingWords)
+                elif matchingCategories[0] == "Happy":
+                    reply = random.choice(happyUnderstandingWords)
+
+                reply += " " + (random.choice(askMoreFeelingQuestions) % matchingCategories[0].lower())
+                return reply
+            
